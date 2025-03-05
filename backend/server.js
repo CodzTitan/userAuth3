df451efb838701d29ed676e5d57cc19b7ebf329b
CodzTitan <codztitan18@gmail.com>
2025-03-05 12:13:15 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,19 +8,8 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors({
-  origin: "https://userauth3.netlify.app"
-  origin: function(origin, callback) {
-    // Allow requests with no origin (like mobile apps or curl requests)
-    if (!origin) return callback(null, true);
-
-    if (allowedOrigins.indexOf(origin) === -1) {
-      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  }
+  origin: 'https://userauth3.netlify.app'
 }));
-
 app.use(express.json());
 
 // MongoDB Connection
